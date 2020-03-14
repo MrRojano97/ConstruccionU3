713a666df7b915452521299f7471b357a3953898
Matias Pizarro <pinkystylee98@gmail.com>
2020-03-14 17:15:11 -0300

    Creacion Canvas Inicial

--- genograma/resources/views/crearGenograma.blade.php
+++ genograma/resources/views/crearGenograma.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp -->
-<br>
+<!--br>
 <br>
 <div class="container">
     <div class="sidenav">
@@ -20,8 +20,54 @@
     </div>
     <a href="#contact">Search</a>
     </div>
-</div>
+</div-->
+<!DOCTYPE html>
 
+<html lang="en", xmlns="https://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8"/>
+        <title></title>
+        <div id="app">
+
+        <script>
+            function init(){
+                var $ = go.GraphObject.make;
+                myDiagram = $(go.Diagram, "myDiagramDiv", {
+                grid: $(go.Panel, "Grid",
+                $(go.Shape, "LineH", { stroke: "lightgray", strokeWidth: 0.5 }),
+                $(go.Shape, "LineH", { stroke: "gray", strokeWidth: 0.5, interval: 10 }),
+                $(go.Shape, "LineV", { stroke: "lightgray", strokeWidth: 0.5 }),
+                $(go.Shape, "LineV", { stroke: "gray", strokeWidth: 0.5, interval: 10 })
+                ),
+                "draggingTool.dragsLink": true,
+                "draggingTool.isGridSnapEnabled": true,
+                "linkingTool.isUnconnectedLinkValid": true,
+                "linkingTool.portGravity": 20,
+                "relinkingTool.isUnconnectedLinkValid": true,
+                "relinkingTool.portGravity": 20,
+                "relinkingTool.fromHandleArchetype":
+                $(go.Shape, "Diamond", { segmentIndex: 0, cursor: "pointer", desiredSize: new go.Size(8, 8), fill: "tomato", stroke: "darkred" }),
+                "relinkingTool.toHandleArchetype":
+                $(go.Shape, "Diamond", { segmentIndex: -1, cursor: "pointer", desiredSize: new go.Size(8, 8), fill: "darkred", stroke: "tomato" }),
+                "linkReshapingTool.handleArchetype":
+                $(go.Shape, "Diamond", { desiredSize: new go.Size(7, 7), fill: "lightblue", stroke: "deepskyblue" }),
+                "rotatingTool.handleAngle": 270,
+                "rotatingTool.handleDistance": 30,
+                "rotatingTool.snapAngleMultiple": 15,
+                "rotatingTool.snapAngleEpsilon": 15,
+                "undoManager.isEnabled": true
+            });
+            }
+        </script>
+        </div>
+
+    </head>
+    <body onload="init()">
+        <div id="myDiagramDiv" style="flex-grow: 1; height:900px; border: solid 1px black"></div>
+    </body>
+
+    <script src="{{asset('js/app.js')}}" defer></script>
+</html>
 @endsection
 
 @extends('layouts.fother')
