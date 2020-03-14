832461904c5a35a966eaeeff6009f48f4cd07a47
René Suazo <renecsc@gmail.com>
2020-03-14 16:12:23 -0300

    Clase Relacion sin implementar

---
+++ genograma/app/Http/Controllers/RelacionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RelacionController extends Controller
+{
+    public $sujetoA;
+    public $sujetoB;
+    public $tipoRelacion;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function getSujetoA(){
+
+    }
+
+    public function setSujetoA(){
+
+    }
+
+    public function getSujetoB(){
+
+    }
+
+    public function setSujetoB(){
+
+    }
+    
+    public function getTipoRelacion(){
+
+    }
+
+    public function setTipoRelacion(){
+
+    }
+}
