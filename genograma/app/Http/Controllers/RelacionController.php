53a0176a723b76f6b3b199c7966065e13edeadc9
René Suazo <renecsc@gmail.com>
2020-03-15 16:21:41 -0300

    se implementaron metodos preliminares

--- genograma/app/Http/Controllers/RelacionController.php
+++ genograma/app/Http/Controllers/RelacionController.php
@@ -15,32 +15,34 @@ class RelacionController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($sujetoA, $sujetoB, $tipoRelacion)
     {
-        
+        $this->sujetoA = $sujetoA;
+        $this->sujetoB = $sujetoB;
+        $this->tipoRelacion = $tipoRelacion;
     }
 
     public function getSujetoA(){
-
+        return $this->sujetoA;
     }
 
-    public function setSujetoA(){
-
+    public function setSujetoA($sujeto){
+        $this->sujetoA = $sujeto;
     }
 
     public function getSujetoB(){
-
+        return $this->sujetoB;
     }
 
-    public function setSujetoB(){
-
+    public function setSujetoB($sujeto){
+        $this->sujetoB = $sujeto;
     }
     
     public function getTipoRelacion(){
-
+        return $this->tipoRelacion;
     }
 
-    public function setTipoRelacion(){
-
+    public function setTipoRelacion($tipo){
+        $this->tipoRelacion = $tipo;
     }
 }
