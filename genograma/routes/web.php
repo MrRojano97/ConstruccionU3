780b0c20c235b594a53b744219cf13692f5d614c
Brayan <brayan.escobar@live.com>
2020-03-16 03:35:19 -0300

    Revert "Crear sujeto"
    This reverts commit 2dea81ffc28c97d7ed729dc21fc24b7f386aaf25.

--- genograma/routes/web.php
+++ genograma/routes/web.php
@@ -20,10 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::post('/rutaSujeto','SujetoController@registrarSujeto' );//metodo para guardar
-Route::get('/rutaSujeto','SujetoController@listarSujetos' );
-Route::get('/rutaSujeto/{post}','SujetoController@listarSujeto' );
-Route::put('/rutaSujeto/{post}','SujetoController@actualizarRegistroSujeto' );
-Route::delete('/rutaSujeto/{post}','SujetoController@eliminarRegistroSujeto' );
