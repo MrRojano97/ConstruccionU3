ca2496e1599aea26810290ebdc3b926b5b866b51
Bastian <bastian.sepiulvedagarcia@gmail.com>
2020-03-16 20:20:33 -0300

    creacion sidebar y componente vue de canvas

--- genograma/resources/views/crearGenograma.blade.php
+++ genograma/resources/views/crearGenograma.blade.php
@@ -2,26 +2,9 @@
 
 @section('content')
 <!-- https://www.w3schools.com/howto/howto_js_dropdown_sidenav.asp -->
-<br>
-<br>
-<div class="container">
-    <div class="sidenav">
-    <a href="#about">About</a>
-    <a href="#services">Services</a>
-    <a href="#clients">Clients</a>
-    <a href="#contact">Contact</a>
-    <button class="dropdown-btn">Dropdown
-        <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-container">
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        <a href="#">Link 3</a>
-    </div>
-    <a href="#contact">Search</a>
-    </div>
-</div>
 
+<sidebar-component></sidebar-component>
+    
 @endsection
 
 @extends('layouts.fother')
