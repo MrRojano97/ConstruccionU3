7087d61fad51aef05f3564aa4415c06293b9729f
Rubén Ramírez <rubenrv95@hotmail.com>
2020-03-23 18:17:46 -0300

    H13: Pop Up al presionar pestaña Dibujar
    Ahora sí aparece. Los botones aún no hacen nada.

--- genograma/resources/js/components/ModalComponent.vue
+++ genograma/resources/js/components/ModalComponent.vue
@@ -1,136 +1,88 @@
 
 
 <template>
-    <div id="modal"> 
-    <body>Como estamos locooooooo </body>
-    <transition name="fade" appear>
-        <div class="modal-overlay " v-if="showModal"></div>
-    </transition>
-    <transition name="slide" appear>
-    <div class="modal" v-if="showModal">
-        <h1>Bienvenido, ¿qué desea hacer?</h1>
-    <button class="button" @click="showModal = false">
-        Nuevo
-    </button>
-    <button class="button" @click="showModal = false">
-        Cargar
-    </button>
+
+<body>
+
+<div class="w3-container">
+  <div id="id01" class="w3-modal">
+    <div class="w3-modal-content">
+      <div class="w3-container">
+        <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+        <p>                  Bienvenido. ¿Qué desea realizar?           </p>
+        <label class="label1"> </label>
+        <button class="button">Nuevo</button>
+        <label class="label2"> </label>
+        <button class="button">Cargar</button>
+        <label class="label1"> </label>
+        </p> 
+      </div>
     </div>
-    </transition>
-    </div>
+  </div>
+</div>
+            
+</body>
 
 </template>
 
-<script>
-    export default {
-        name: "modal",
-        data() {
-            return {
-                showModal: true
-            }
-        },
-        mounted() {
-            console.log("Wena cabros")
-        }
-    }
-</script>
-
 <style>
-
-
-    body {
-        font-family: 'monserrat', sans-serif,
-    }
-
-    #modal {
-        position: relative;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        width: 100vw;
-        height: 100vh;
-        overflow-x: hidden;
-
-    }
-
-    .button {
+    .button{
         appearance: none;
         outline: none;
         border: none;
         background: none;
         cursor: pointer;
-        padding: 15px 25px;
-        display: inline-block;
 
+        
+        display: inline-block;
+        padding: 15px 25px;
+        background-color: #00008B;
+        border-radius: 8px;
         color: #FFF;
-        font-size: 18px
-        font-weight: 700;
+        font-size: 18px;
+        font-weight: 200;
         
         box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
         transition: 0.4s ease-out;
         
         &:hover {
-            box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+        box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
         }
     }
 
-    .modal-overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: 98;
-        background-color: rgba(0, 0, 0, 0.3);
-    }
-
-    .modal {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 99;
+    .label1 {
         
-        width: 100%;
-        max-width: 400px;
-        background-color: #FFF;
-        border-radius: 16px;
-        
-        padding: 25px;
-    
-    h1 {
-    color: #222;
-    font-size: 32px;
-    font-weight: 900;
-    margin-bottom: 15px;
-    }
-    
-    p {
-    color: #666;
-    font-size: 18px;
-    font-weight: 400;
-    margin-bottom: 15px;
-    }
+        width: 35%;
     }
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity .5s;
-    }
-
-    .fade-enter,
-    .fade-leave-to {
-        opacity: 0;
-    }
-
-    .slide-enter-active,
-    .slide-leave-active {
-        transition: transform .5s;
-    }
-
-    .slide-enter,
-    .slide-leave-to {
-        transform: translateY(-50%) translateX(100vw);
+    .label2 {
+        width: 20px;
     }
 </style>
+
+
+<script>
+
+    $(document).ready(function(){
+
+    });
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        mounted() {
+            console.log('Wena los ka')
+            this.openModal();
+        },
+        methods: {
+            openModal() {
+                document.getElementById('id01').style.display='block';
+            }
+        },
+        beforeMount() {
+            this.openModal();
+        },
+    }
+</script>
