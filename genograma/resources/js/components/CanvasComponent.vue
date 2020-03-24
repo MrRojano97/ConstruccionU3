7e263056a25f446ca86e9170e0261150333a683a
Matias Escobar <mescobar13@alumnos.utalca.cl>
2020-03-23 23:24:16 -0300

    implementar integración de botón guardar diagrama con rutas laravel (H13)

--- genograma/resources/js/components/CanvasComponent.vue
+++ genograma/resources/js/components/CanvasComponent.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="myDiagramDiv" id="myDiagramDiv"></div>
         <div class="button-container">
-            <button class="button" @click="guardarDiagrama()">Guardar</button>
+            <button class="button" @click="guardarDiagrama">Guardar</button>
         </div>
     </div>
 </template>
@@ -37,17 +37,49 @@
 
 </style>
 <script>
+    const baseUrl = "http://127.0.0.1:8000/";
     export default {
-        data (){
-            return {
-                // Acá debería ir declarado el mapa (o json) que representa el diagrama
-            }
+        created: function(){
+            // esta función se ejecuta al cargar el componente
+            // implementar alguna forma de setear los datos del usuario actual.
+            // se supone que en la ventana modal se deberían cargar los datos del sujeto
+
+            //this.getUsuarioActual(); //método tentativo
+        },
+        data:  {
+            // Acá debería ir declarado el mapa (o json) que representa el diagrama
+            //estos datos corresponden al usuario actual
+            nombreUsuarioActual : '',
+            apellidoUsuarioActual : '',
+            edadUsuarioActual : '',
+            generoUsuarioActual : '',
+            diagramaUsuarioActual : {}
         },
         methods: {
-            guardarDiagrama() {
-                console.log("guardando diagrama!");
-                //TODO: implementar aquí llamado a ruta para guardar el diagrama
+            guardarDiagrama : function() {
+                axios.post(baseUrl+"rutaSujeto", {
+                    'nombre' : this.nombreUsuarioActual,
+                    'apellido' : this.apellidoUsuarioActual,
+                    'edad' : this.edadUsuarioActual,
+                    'genero' : this.generoUsuarioActual,
+                    'diagrama' : this.diagramaUsuarioActual,
+                }).then(response => {
+                    console.log("---> RECIBIENDO DESDE GUARDAR DIAGRAMA:" + response);
+                });
             },
+            setDiagramaActual : function(diagrama){
+              this.diagramaUsuarioActual = diagrama;
+            },
+            getUsuarioActual : function (){
+                // Esta función debería ser algo asi:
+                // axios.get(baseUrl+"rutaSujeto").then(response => {
+                //     this.nombreUsuarioActual = response.nombre;
+                //     thislapellidoUsuarioActual = response.apellido;
+                //     this.edadUsuarioActual = response.edad;
+                //     this.greneroUsuarioActual = response.genero;
+                //     this.diagramaUsuarioActual = response.diagrama;
+                // });
+            }
         }
     }
 </script>
